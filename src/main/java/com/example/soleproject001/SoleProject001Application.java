fa2462b8083f91eace21c9decd8458f89a32e96c
ohyoubee <dhdbql1022@gmail.com>
2024-01-22 16:02:46 +0900

    Initial commit

---
+++ src/main/java/com/example/soleproject001/SoleProject001Application.java
@@ -0,0 +1,13 @@
+package com.example.soleproject001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoleProject001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoleProject001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/soleproject001/SoleProject001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soleproject001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoleProject001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
